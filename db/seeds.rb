ed683228af6b27c1d1e25038274f5d793ad98241
uwadonat <uwadonat@gmail.com>
2021-11-02 12:38:17 +0300

    Create table to strore five greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroy previous greetings"
+
+Message.destroy_all
+
+puts "Five new greetings are created"
+
+messages = Message.create([ 
+    { body: 'Hello, good morging my good friend' },
+    { body: 'Hello, I will be a bit late today' },
+    { body: 'Hello, I will buy food for everyone' }, 
+    { body: 'Bashh! I\'m tired like a fool' },
+    { body: 'Hey, the code is full of bugs' },
+     ]) 
